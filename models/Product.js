c2214db73fb57effd30adbdfc00e0e63f4554ace
elliotzacharywaxman <elliot@climatedao.xyz>
2022-06-16 08:38:01 -0400

    added skelaton to model and routes

---
+++ models/Product.js
@@ -0,0 +1,35 @@
+// import important parts of sequelize library
+const { Model, DataTypes, INTEGER } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+const Category = require('./Category');
+
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Product extends Model {}
+
+// set up fields and rules for Product model
+Product.init(
+  {
+    // define columns
+    id:{
+
+    },
+    category_id:{
+      type:DataTypes.INTEGER,
+      references:{
+        model: "category",
+        key: "id"
+      }
+
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  }
+);
+
+module.exports = Product;
